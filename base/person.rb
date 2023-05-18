2852fdc9cd8fef42d060e934ae3c3982cedf339e
jolak5 <kayodej4@gmail.com>
2023-05-18 12:52:42 +0100

    implement reviewer's changes

--- base/person.rb
+++ base/person.rb
@@ -22,9 +22,8 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(rental)
-    rental.person = self unless rental.person.include?(self)
-    @rentals << rental
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private
