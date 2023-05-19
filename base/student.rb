079dcf9e89a5d2db2b6f6ee448378e3fa1dde450
jolak5 <kayodej4@gmail.com>
2023-05-19 20:35:26 +0100

    fix linters

--- base/student.rb
+++ base/student.rb
@@ -5,7 +5,7 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(age, name = 'Unknown', parent_permission: true, classroom: 'Unknown')
-    super(age, name, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
